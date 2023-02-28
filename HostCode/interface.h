8d75a796ffa751654da71bc0293a6f975ffeff27
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-02-28 09:32:04 +0000

    Merge deauth detect in with regular sniffer

--- HostCode/interface.h
+++ HostCode/interface.h
@@ -2,7 +2,6 @@
 #include "beaconSniffer.h"
 #include "beaconSpammer.h"
 #include "deauther.h"
-#include "deauthDetector.h"
 
 class ESP32Interface{
 private:
@@ -12,7 +11,6 @@ private:
     beaconSniffer sniffer;
     beaconSpammer spammer;
     deauther deauth;
-    deauthDetector deauthDetect;
 
 public:
     ESP32Interface(std::string port="/dev/ttyUSB0", int baudrate=B115200){
@@ -21,7 +19,6 @@ public:
         this->sniffer.config(&this->networks, port, baudrate);
         this->spammer.config(port, baudrate);
         this->deauth.config(&this->networks, port, baudrate);
-        this->deauthDetect.config(&this->networks, port, baudrate);
     }
     
     ~ESP32Interface(){
@@ -32,7 +29,6 @@ public:
         this->sniffer.draw(0, 0.51, 0.25, 1, ImGuiCond_Always);
         this->spammer.draw(0.26, 0.51, 0.5, 1, ImGuiCond_Always);
         this->deauth.draw(0.51, 0.51, 0.75, 1, ImGuiCond_Always);
-        this->deauthDetect.draw(0.76, 0.51, 1, 1, ImGuiCond_Always);
     }
 
 };
